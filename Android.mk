194da951bcd53c34b5a7c12fc6c9ba424af08679
Alex <alex@dirtyunicorns.com>
2018-06-27 07:43:12 -0500

    Modify paths to avoid issues inheriting it into the fwb
    - Fix merge conflict

--- Android.mk
+++ Android.mk
@@ -1,27 +1,8 @@
-<<<<<<< HEAD
-#LOCAL_PATH:= $(call my-dir)
-#
-#include $(CLEAR_VARS)
-#
-#LOCAL_JAVA_LIBRARIES := org.dirtyunicorns.utils
-#
-#LOCAL_STATIC_JAVA_LIBRARIES := \
-#    trail-drawing \
-#    rebound
-#
-#LOCAL_SRC_FILES := $(call all-java-files-under, src)
-#
-#LOCAL_MODULE_TAGS := optional
-#
-#LOCAL_MODULE := org.dirtyunicorns.navigation-static-internal
-#
-#include $(BUILD_STATIC_JAVA_LIBRARY)
-=======
 LOCAL_PATH:= $(call my-dir)
 
 include $(CLEAR_VARS)
 
-LOCAL_SRC_FILES := $(call all-java-files-under, src)
+LOCAL_SRC_FILES := $(call all-java-files-under, src/com/android/internal)
 
 LOCAL_MODULE_TAGS := optional
 
@@ -29,5 +10,4 @@ LOCAL_MODULE := org.dirtyunicorns.utils
 
 include $(BUILD_JAVA_LIBRARY)
 
->>>>>>> 673ad575e0ffb538389395d9a2afcf558e9a4c09
 
